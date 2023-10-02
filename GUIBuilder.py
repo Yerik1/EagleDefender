47629a76fb516529c9c20126af515017f6c24a86
unknown <luisalejandrovb@gmail.com>
2023-10-02 03:12:03 -0600

    Implementacion del interfaz del ambiente inicial y modificacion de generador de interfaz

--- GUIBuilder.py
+++ GUIBuilder.py
@@ -1,21 +1,32 @@
+# Imports
 from tkinter import *
 import tkinter as tk
+from PIL import Image, ImageTk
 
 class GUIBuilder:
     # Metodo constructor de la clase
-    def __init__(self):
+    def __init__(self,BG):
 
         # Crea una nueva ventana de Tkinter
         self.root = Tk()
 
+        # Color de fondo de la ventana
+        self.BG = BG
+
+        #asigna el color de fondo de la ventana
+        self.root.config(background=self.BG)
+
         # Obtener las dimensiones de la pantalla
-        width = self.root.winfo_screenwidth() # Ancho
-        height = self.root.winfo_screenheight() # Altura
+        width = self.root.winfo_screenwidth()  # Ancho
+        height = self.root.winfo_screenheight()  # Altura
 
         # Configurar la ventana en pantalla completa
         self.root.attributes("-fullscreen", True)
 
-        self.buttons("X", self.closeEnvironment, "Red","Blue", width-15, 0)
+        # Crea botones para cerrar o minimizar la ventana
+        self.buttons(" x ", self.closeEnvironment, "Red","Blue", width-23, -0.5)
+        self.buttons(" - ", self.minimize, "Red", "Blue", width-45, -0.5)
+
 
 
     # Metodo encargado de iniciar el bucle de la ventana
@@ -23,25 +34,50 @@ class GUIBuilder:
         self.root.mainloop()
 
 
+    # Metodo que retorna el color de fondo de la ventana
+    def getBG(self,BG):
+        return self.BG
+        #self.root.config(background = BG)
+
+
+    # Agrega canvas y define el fondo y el borde
+    def addCanvas(self, w, h, a, b,BG):
+        canvas = Canvas(self.root, width=w, height=h, bg=self.BG,  highlightbackground=BG)
+        canvas.place(x=a, y=b)
+        return canvas
+
+
+    def addLabel(self, txt, a, b, r):
+        label = Label(self.root, text=txt, bg=self.BG, relief=r)
+        label.place(x=a, y=b)
+
+
     # Metodo constructor de botones
     def buttons(self, title, action, color1,color2, a, b):
 
-        btn = Button(self.root,text = title, command = action)
+        btn = Button(self.root, text=title, command=action, bd=0, relief="sunken", activebackground=self.BG)
         btn.config(bg=color1)
         btn.place(x=a, y=b)
 
-        # Define el fondo del boton
+        # Define el fondo del boton cuando el puntero sale
         def change1(event):
-            btn.config(bg = color1)
+            btn.config(bg=color1)
 
-        # Define el fondo del boton
+        # Define el fondo del boton cuando el puntero entra
         def change2(event):
-            btn.config(bg = color2)
+            btn.config(bg=color2)
 
         # Cambia el color del boton cuando el puntero pasa por este
-        btn.bind("<Leave>", change1)
-        btn.bind("<Enter>", change2)
+        btn.bind("<Leave>", change1)  # Sale
+        btn.bind("<Enter>", change2)  # Entra
+
 
     # Metodo que cierra la ventana
     def closeEnvironment(self):
         self.root.destroy()
+
+    # Metodo que minimiza la ventana
+    def minimize(self):
+        self.root.iconify()
+
+
