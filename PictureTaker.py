ebe98034e48289ce5d63f823e55d1020ccd0859e
Yerik Chaves S <yerikchaves818@gmail.com>
2023-10-21 17:08:59 -0600

    prueba2

---
+++ PictureTaker.py
@@ -0,0 +1,60 @@
+import cv2
+from tkinter import *
+from PIL import Image, ImageTk
+
+class CamApp:
+    def __init__(self,canvas):
+        self.window = Tk()
+        self.window.title("Cámara en Vivo")
+        self.status=True
+        self.path=""
+        self.cap = cv2.VideoCapture(0)
+        if not self.cap.isOpened():
+            print("Error: No se pudo abrir la cámara.")
+            self.window.destroy()
+            return
+        self.canva=canvas
+
+        self.capButton = Button(self.window, text="Capturar Foto", command=self.takePicture)
+        self.capButton.pack()
+        self.window.protocol("WM_DELETE_WINDOW", self.exit)
+        self.update()
+
+
+
+    def takePicture(self):
+        ret, frame = self.cap.read()
+        if ret:
+            self.path="Temp/foto_capturada.png"
+            cv2.imwrite(self.path, frame)
+            print("Foto capturada y guardada como 'foto_capturada.png'.")
+            self.exit()
+
+
+    def update(self):
+        ret, frame = self.cap.read()
+        if ret:
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            image = Image.fromarray(frame)
+            # Redimensionar la imagen para que se ajuste a la ventana
+            image = image.resize((200, 200))
+            imageTk = ImageTk.PhotoImage(image)
+            self.canva.config(image=imageTk)
+            self.canva.imagen_tk = imageTk
+            self.window.after(10, self.update)
+        else:
+            print("Error: No se pudo capturar el fotograma.")
+            self.exit()
+
+    def exit(self):
+        self.cap.release()
+        cv2.destroyAllWindows()
+        self.window.quit()
+        self.window.destroy()
+        self.status=False
+
+    def begin(self):
+        self.window.mainloop()
+        while (True):
+            if not self.status:
+                return self.path
---
+++ TranslationManager.py
@@ -0,0 +1,52 @@
+# TranslationManager.py
+class TranslationManager:
+    def __init__(self):
+        self.translations = {
+            'English': {
+                'wallsLb': ' Walls:',
+                'powersLb': ' Powers',
+                'pointsDefender': ' Points',
+                'pointsAttacker': ' Points',
+                'userDefender': ' User',
+                'userAttacker': ' User',
+                'musicDefender': ' Music',
+                'musicAttacker': ' Music',
+                'funFactDefender': ' Fun Facts: ...',
+                'funFactAttacker': ' Fun Facts: ...',
+                'rolDefender': ' Defender',
+                'rolAttacker': ' Attacker',
+                #'profilePicDefender': ' Profile Pic',
+                'profilePicAttacker': ' Profile Pic',
+            },
+            'Español': {
+                'wallsLb': ' Paredes',
+                'powersLb': ' Poderes',
+                'pointsDefender': ' Puntos',
+                'pointsAttacker': ' Puntos',
+                'userDefender': '      Usuario',
+                'userAttacker': ' Usuario',
+                'musicDefender': '      Música',
+                'musicAttacker': ' Música',
+                'funFactDefender': ' Datos Curiosos: ...',
+                'funFactAttacker': ' Datos Curiosos: ...',
+                'rolDefender': ' Defensor',
+                'rolAttacker': ' Atacante',
+                #'profilePicDefender': ' Foto de Perfil',
+                'profilePicAttacker': ' Foto de Perfil',
+            },
+        }
+        self.currentLanguage = 'English'
+
+    def loadTranslations(self):
+        # Carga las traducciones desde algún archivo o base de datos si es necesario
+        pass
+
+    def setLanguage(self, language):
+        self.currentLanguage = language
+
+    def updateWidgets(self, widgetDict):
+        language = self.currentLanguage
+        for widgetId, translation in self.translations[language].items():
+            widget = widgetDict.get(widgetId)
+            if widget:
+                widget.config(text=translation)
